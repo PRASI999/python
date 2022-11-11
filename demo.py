382dde8dbb61366d40b5e627cec877fb1c696a8a
p,penchala <ppprasad90@gmail.com>
2022-11-11 16:09:10 +0530

    first file

---
+++ demo.py
@@ -0,0 +1,11 @@
+a = int(input("enter vavlue for a="))
+b = int(input("enter vavlue for b="))
+s = a+b
+p = a-b
+q = a*b
+r = a/b
+print("sum of" ,a, "and" ,b, "=" ,s)
+print("sub of" ,a, "and" ,b, "=" ,p)
+print("mul of" ,a, "and" ,b, "=" ,q)
+print("div of" ,a, "and" ,b, "=" ,r)
+
